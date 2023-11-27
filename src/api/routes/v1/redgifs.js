1397f50e8e63ae45ab2dbc7cc8e175118a9d797e
Valtorein <152112236+Valtorein@users.noreply.github.com>
2023-11-27 21:07:28 +0000

    Remove console logs

--- src/api/routes/v1/redgifs.js
+++ src/api/routes/v1/redgifs.js
@@ -7,7 +7,7 @@ const axios = Axios.create();
 
 router.get("/", async (req, res) => {
   const { id, token } = req.query;
-  console.log("redgifs", id);
+
   const response = await axios.get(
     `https://api.redgifs.com/v2/gifs/${id}`,
     {
@@ -16,21 +16,18 @@ router.get("/", async (req, res) => {
       }
     }
   );
-  console.log("response", response.data.gif);
+
   const imgResponse = await axios.get(response.data.gif.urls.hd, {
     responseType: "stream",
     headers: {
       authorization: `Bearer ${token}`,
     }
   });
-  console.log("asdf");
   const filename = path.basename(response.data.gif.urls.hd);
-  console.log(imgResponse);
 
   res.setHeader("content-disposition", `attachment; filename="${filename}"`);
   // pipe the data to the res object
   imgResponse.data.pipe(res);
-  // res.send('oop');
 });
 
 module.exports = router;
