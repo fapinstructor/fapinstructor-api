50b38abe3d0dcceaa77f6b5d4a9777579ffed502
Valtorein <152112236+Valtorein@users.noreply.github.com>
2023-12-09 14:38:53 +0000

    Update redgifs lookup to forward the user agent header with the request and return the data object instead of getting the image

--- src/api/routes/v1/redgifs.js
+++ src/api/routes/v1/redgifs.js
@@ -1,33 +1,23 @@
 const { Router } = require("express");
 const Axios = require('axios');
-const path = require('path');
 
 const router = Router();
 const axios = Axios.create();
 
 router.get("/", async (req, res) => {
-  const { id, token } = req.query;
+  const { id } = req.query;
 
   const response = await axios.get(
     `https://api.redgifs.com/v2/gifs/${id}`,
     {
       headers: {
-        authorization: `Bearer ${token}`,
+        authorization: req.headers.authorization,
+        'User-Agent': req.headers["user-agent"],
       }
     }
   );
 
-  const imgResponse = await axios.get(response.data.gif.urls.hd, {
-    responseType: "stream",
-    headers: {
-      authorization: `Bearer ${token}`,
-    }
-  });
-  const filename = path.basename(response.data.gif.urls.hd);
-
-  res.setHeader("content-disposition", `attachment; filename="${filename}"`);
-  // pipe the data to the res object
-  imgResponse.data.pipe(res);
+  res.send(response.data.gif);
 });
 
 module.exports = router;
